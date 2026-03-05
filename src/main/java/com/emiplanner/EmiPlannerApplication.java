4b9c3f2beaabd1aadffa2ab2b979090d6591b8fd
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2026-03-05 20:08:21 +0530

    chore: initialize EMI Planner Spring Boot project with core dependencies

---
+++ src/main/java/com/emiplanner/EmiPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.emiplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmiPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emiplanner/EmiPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emiplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmiPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
